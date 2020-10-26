2dbc48e4ea1547e6a4b7b14a697e2d8f911610ae
ACT3 <chuiC456@163.com>
2020-10-27 00:24:00 +0800

    Add prototype of tbg-insert-header command.

--- tbg-header.el
+++ tbg-header.el
@@ -1,66 +1,114 @@
-; tbg-header.el --- 
-; 
-; Filename: tbg-header.el
-; Description: 
-; Author: chuic456
-; Maintainer: 
-; Created: Sun Oct 25 13:29:33 2020 (+0800)
-; Version: 
-; Package-Requires: ()
-; Last-Updated: 
-;           By: 
-;     Update #: 0
-; URL: 
-; Doc URL: 
-; Keywords: 
-; Compatibility: 
-; 
-; 
+;;; tbg-header.el --- ▮ -*- coding: utf-8; lexical-binding: t; -*-
 
-; Commentary: 
-; 
-; 
-; 
-; 
+;; Copyright © 2020-2027, by 3badguys
 
-; Change Log:
-; 
-; 
-; 
-; 
-; This program is free software: you can redistribute it and/or modify
-; it under the terms of the GNU General Public License as published by
-; the Free Software Foundation, either version 3 of the License, or (at
-; your option) any later version.
-; 
-; This program is distributed in the hope that it will be useful, but
-; WITHOUT ANY WARRANTY; without even the implied warranty of
-; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-; General Public License for more details.
-; 
-; You should have received a copy of the GNU General Public License
-; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
-; 
-; 
+;; Author: 3badguys
+;; Version: ▮
+;; Created: ▮
+;; Package-Requires: ▮
+;; Keywords: ▮
+;; License: GPL v3
 
-; Code:
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;;; Code:
 
 
+(defun insert-title ()
+  "Insert titile line."
+  (insert (concat ";;; "
+                  (if (buffer-file-name)
+                      (file-name-nondirectory (buffer-file-name))
+                    (buffer-name))
+                  " --- ▮ -*- coding: utf-8; lexical-binding: t; -*-\n\n"
+                  )))
+
+(defun insert-copyright ()
+  "Insert copyright line."
+  (interactive)
+  (insert ";; Copyright © 2020-2027, by 3badguys\n\n"))
+
+(defun insert-author ()
+  "Insert author line."
+  (interactive)
+  (insert ";; Author: 3badguys\n"))
+
+(defun insert-version ()
+  "Insert version line."
+  (interactive)
+  (insert ";; Version: ▮\n"))
+
+(defun insert-create-date ()
+  "Insert create date line."
+  (interactive)
+  (insert ";; Created: ▮\n"))
+
+(defun insert-package-requires ()
+  "Insert package requires line."
+  (interactive)
+  (insert ";; Package-Requires: ▮\n"))
+
+(defun insert-keyword ()
+  "Insert keyword line."
+  (interactive)
+  (insert ";; Keywords: ▮\n"))
+
+(defun insert-license ()
+  "Insert license line."
+  (interactive)
+  (insert ";; License: GPL v3\n\n"))
+
+(defun insert-not-gnu-emacs ()
+  "Insert not gnu emacs line."
+  (interactive)
+  (insert ";; This file is not part of GNU Emacs.\n\n"))
+
+(defun insert-commentary ()
+  "Insert commentary line."
+  (interactive)
+  (insert ";;; Commentary:\n\n"))
+
+(defun insert-code ()
+  "Insert code line."
+  (interactive)
+  (insert ";;; Code:\n\n"))
+
+(defun insert-end ()
+  "Insert end line."
+  (interactive)
+  (insert (concat "\n;;; "
+                  (if (buffer-file-name)
+                      (file-name-nondirectory (buffer-file-name))
+                    (buffer-name))
+                  " ends here\n"
+                  )))
+
+;;;###autoload
 (defun tbg-insert-header ()
   "Insert the file header."
   (interactive)
-  (let ()
-3
- ))
-
-(defun tbg-update-header ()
-  "Update the file header."
-  (interactive)
-  (let ()
-3
- ))
+  (save-excursion
+    ;;
+    (beginning-of-buffer)
+    (insert-title)
+    (insert-copyright)
+    (insert-author)
+    (insert-version)
+    (insert-create-date)
+    (insert-package-requires)
+    (insert-keyword)
+    (insert-license)
+    (insert-not-gnu-emacs)
+    (insert-commentary)
+    (insert-code)
+    ;;
+    (end-of-buffer)
+    (insert-end)
+    ;;
+    ))
 
 (provide 'tbg-header)
 
-; 
-; tbg-header.el ends here
+;;; tbg-header.el ends here

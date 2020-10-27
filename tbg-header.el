c9108d889a9fad7d5d3fb8d90515442ee3d37524
ACT3 <chuiC456@163.com>
2020-10-28 00:47:30 +0800

    Add prototype of tbg-remove-header command.

--- tbg-header.el
+++ tbg-header.el
@@ -98,6 +98,45 @@
     ;;
     ))
 
+;;;###autoload
+(defun tbg-remove-header ()
+  "Remove the file header."
+  (interactive)
+  (let (
+        $titile-str $code-str $end-str
+        $header-p1 $header-p2 $end-p1 $end-p2)
+    (save-excursion
+      (setq $titile-str (concat ";;; "
+                                (if (buffer-file-name)
+                                    (file-name-nondirectory (buffer-file-name))
+                                  (buffer-name))
+                                " --- "
+                                ))
+      (setq $code-str ";;; Code:\n\n")
+      (setq $end-str (concat "\n;;; "
+                             (if (buffer-file-name)
+                                 (file-name-nondirectory (buffer-file-name))
+                               (buffer-name))
+                             " ends here\n"
+                             ))
+      ;;
+      (beginning-of-buffer)
+      (search-forward $titile-str nil t)
+      (search-backward $titile-str nil t)
+      (setq $header-p1 (point))
+      (search-forward $code-str nil t)
+      (setq $header-p2 (point))
+      (delete-region $header-p1 $header-p2)
+      ;;
+      (end-of-buffer)
+      (search-backward $end-str nil t)
+      (setq $end-p1 (point))
+      (search-forward $end-str nil t)
+      (setq $end-p2 (point))
+      (delete-region $end-p1 $end-p2)
+      ;;
+      )))
+
 (provide 'tbg-header)
 
 ;;; tbg-header.el ends here
